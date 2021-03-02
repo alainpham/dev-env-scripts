d7fcc000fb83cb5ff9fbb795272dafcf53748da9
Alain Pham <alainphm@gmail.com>
2021-03-02 13:18:20 +0100

    working hornet

--- demo-apps/hornetq-client/src/main/java/demo/Application.java
+++ demo-apps/hornetq-client/src/main/java/demo/Application.java
@@ -29,31 +29,31 @@ public class Application {
         // THIS WORKS
 
 
-        // params.put("host", "amqbrokera0");
-        // params.put("port", 5446);
-        // params.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
-        // params.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/WORKSPACES/ws-dev-env-as-code/amqbroker/tls/truststore.jks");
-        // params.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME,"password");
-        // params.put(TransportConstants.TRUSTSTORE_PROVIDER_PROP_NAME,"JKS");
-
-        // params.put(TransportConstants.KEYSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/WORKSPACES/ws-dev-env-as-code/amqbroker/tls/keystore.p12");
-        // params.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME,"password");
-        // params.put(TransportConstants.KEYSTORE_PROVIDER_PROP_NAME,"JKS");
-
-        //
-
-        params.put("host", "amq-broker-a-hornetq-0-svc-rte-amq-messaging-a.apps.cluster-fbdc.fbdc.example.opentlc.com");
-        params.put("port", "443");
+        params.put("host", "amqbrokera0");
+        params.put("port", 5446);
         params.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
-        params.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/RUN/apps/messaging-tester/tls/truststore.jks");
+        params.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/WORKSPACES/ws-dev-env-as-code/amqbroker/tls/truststore.jks");
         params.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME,"password");
         params.put(TransportConstants.TRUSTSTORE_PROVIDER_PROP_NAME,"JKS");
 
-        params.put(TransportConstants.KEYSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/RUN/apps/messaging-tester/tls/keystore.jks");
+        params.put(TransportConstants.KEYSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/WORKSPACES/ws-dev-env-as-code/amqbroker/tls/keystore.p12");
         params.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME,"password");
         params.put(TransportConstants.KEYSTORE_PROVIDER_PROP_NAME,"JKS");
+
+        //
+
+        // params.put("host", "amq-broker-a-hornetq-0-svc-rte-amq-messaging-a.apps.cluster-fbdc.fbdc.example.opentlc.com");
+        // params.put("port", "443");
+        // params.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
+        // params.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/RUN/apps/messaging-tester/tls/truststore.jks");
+        // params.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME,"password");
+        // params.put(TransportConstants.TRUSTSTORE_PROVIDER_PROP_NAME,"JKS");
+
+        // params.put(TransportConstants.KEYSTORE_PATH_PROP_NAME,"/home/workdrive/TAZONE/RUN/apps/messaging-tester/tls/keystore.jks");
+        // params.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME,"password");
+        // params.put(TransportConstants.KEYSTORE_PROVIDER_PROP_NAME,"JKS");
         
-        params.put(TransportConstants.HTTP_ENABLED_PROP_NAME,true);
+        // params.put(TransportConstants.HTTP_ENABLED_PROP_NAME,true);
         
         TransportConfiguration config = new TransportConfiguration(NettyConnectorFactory.class.getName(), params);
         HornetQConnectionFactory factory = HornetQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, config);
